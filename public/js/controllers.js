83c562e5f7a63dfff16dab1cb03bac2eccd51d0a
bertolomych <rom.bekk@gmail.com>
2014-10-26 21:38:05 +0300

    saveing inbox to local storage

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,23 @@
 var scControllers = angular.module('scControllers', []);
 
 scControllers.controller('inbox', function($scope){
-	$scope.inbox = [];
+	$scope.inbox = getFromLocalStorage();
+
+	function saveToLocalStorage(){
+		localStorage.setItem('inbox', angular.toJson($scope.inbox));
+	}
+
+	function getFromLocalStorage(){
+		var inbox = localStorage.getItem('inbox');
+		if (inbox) return JSON.parse(inbox);
+		return [];
+	}
 
 	$scope.newThing = '';
 	$scope.addNewThing = function(){
 		if($scope.newThing === '') return;
 		$scope.inbox.push({title: $scope.newThing, created: Date.now()});
 		$scope.newThing = '';
+		saveToLocalStorage();
 	};
 });
