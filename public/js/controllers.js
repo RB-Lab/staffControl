77673c6c1d076fbea233e7595eb94a12c5eded73
bertolomych <rom.bekk@gmail.com>
2014-10-26 21:25:35 +0300

    the simpliest 'add to inbox'

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,1 +1,12 @@
 var scControllers = angular.module('scControllers', []);
+
+scControllers.controller('inbox', function($scope){
+	$scope.inbox = [];
+
+	$scope.newThing = '';
+	$scope.addNewThing = function(){
+		if($scope.newThing === '') return;
+		$scope.inbox.push({title: $scope.newThing, created: Date.now()});
+		$scope.newThing = '';
+	};
+});
