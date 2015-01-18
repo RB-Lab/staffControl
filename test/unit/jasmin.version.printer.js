1e1b516a5db85b2be431db77c46e015f46dc998e
bertolomych <rom.bekk@gmail.com>
2015-01-18 20:15:17 +0300

    some jshint changes

--- test/unit/jasmin.version.printer.js
+++ test/unit/jasmin.version.printer.js
@@ -1,3 +1,4 @@
+/* global console */
 // Just print jasmine version to use right docs.
 // originaly got form here: http://stackoverflow.com/a/24919927/1105860
 if (jasmine.version) { //the case for version 2.0.0
